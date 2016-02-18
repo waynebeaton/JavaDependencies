85b75c77e9366391eb13eed93262b43d6d55441d
Wayne Beaton <wayne@eclipse.org>
2016-02-18 16:58:35 -0500

    Add file headers.

--- bundles/org.eclipselabs.jdt.modules/src/org/eclipselabs/jdt/modules/views/ModuleReadTreeView.java
+++ bundles/org.eclipselabs.jdt.modules/src/org/eclipselabs/jdt/modules/views/ModuleReadTreeView.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2015 The Eclipse Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eclipselabs.jdt.modules.views;
 
 import java.io.IOException;
@@ -17,25 +24,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jdt.core.IJavaProject;
-
-
-/**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
- */
 
 public class ModuleReadTreeView extends ViewPart {
 
